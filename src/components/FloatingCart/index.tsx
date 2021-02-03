7e4251eb9054861bd647c6bd948aa7f820cab7c2
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-02-03 13:12:42 -0300

    configurando FloatingCart

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const sum = products.reduce((total, product) => {
+      return total + product.price * product.quantity;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(sum);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalQuantity = products.reduce((total, product) => {
+      return total + product.quantity;
+    }, 0);
 
-    return 0;
+    return totalQuantity;
   }, [products]);
 
   return (
